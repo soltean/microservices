36aac23a7870115978fe864f51481b0434cea53e
Sergiu Oltean <sergiu.c.oltean@gmail.com>
2017-09-25 20:46:55 +0300

    updates.

--- api-gateway/src/main/java/com/so/filter/ItemCacheSaveFilter.java
+++ api-gateway/src/main/java/com/so/filter/ItemCacheSaveFilter.java
@@ -1,61 +1,61 @@
 package com.so.filter;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import javax.servlet.ServletInputStream;
-import javax.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.util.StreamUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.so.model.ItemRequest;
 import com.so.service.CacheService;
-import static com.so.config.FiltersOrder.ITEM_CACHE_SAVE_FILTER;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.charset.Charset;
 
 @Component
 public class ItemCacheSaveFilter extends ZuulFilter {
 
-	private CacheService cacheService;
+    private CacheService cacheService;
 
-	@Autowired
-	private ItemCacheSaveFilter(CacheService cacheService) {
-		this.cacheService = cacheService;
-	}
+    @Autowired
+    private ItemCacheSaveFilter(CacheService cacheService) {
+        this.cacheService = cacheService;
+    }
 
-	@Override
-	public String filterType() {
-		return "post";
-	}
+    @Override
+    public String filterType() {
+        return "post";
+    }
 
-	@Override
-	public int filterOrder() {
-		return 1;
-	}
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
 
-	@Override
-	public boolean shouldFilter() {
-		RequestContext ctx = RequestContext.getCurrentContext();
-		HttpServletRequest request = ctx.getRequest();
-		return request.getMethod().equals("POST") && request.getRequestURI().contains("items");
-	}
+    @Override
+    public boolean shouldFilter() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        return request.getMethod().equals("POST") && request.getRequestURI().contains("items");
+    }
 
-	@Override
-	public Object run() {
-		RequestContext ctx = RequestContext.getCurrentContext();
-		int responseStatus = ctx.getResponse().getStatus();
-		if (responseStatus == 200) {
-			try {
-				ServletInputStream request = ctx.getRequest().getInputStream();
-				String req = StreamUtils.copyToString(request, Charset.defaultCharset());
-				ItemRequest itemRequest = new ObjectMapper().readValue(req, ItemRequest.class);
-				cacheService.saveItemToCache(itemRequest.getItemCode(), itemRequest);
-			} catch (IOException e) {
-				//log something - out of scope
-			}
-		}
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        int responseStatus = ctx.getResponse().getStatus();
+        if (responseStatus == 200) {
+            try {
+                ServletInputStream request = ctx.getRequest().getInputStream();
+                String req = StreamUtils.copyToString(request, Charset.defaultCharset());
+                ItemRequest itemRequest = new ObjectMapper().readValue(req, ItemRequest.class);
+                cacheService.saveItemToCache(itemRequest.getItemCode(), itemRequest);
+            } catch (IOException e) {
+                //log something - out of scope
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 }
