9f59d19e265c02548aecfd3cd418b8a67889a0e7
Sergiu Oltean <sergiu.c.oltean@gmail.com>
2017-10-04 23:36:05 +0300

    fixes.

--- zipkin/src/main/java/com/so/app/Application.java
+++ zipkin/src/main/java/com/so/app/Application.java
@@ -1,11 +1,13 @@
 package com.so.app;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin.server.EnableZipkinServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableZipkinServer
+@EnableEurekaClient
 public class Application {
 
 	public static void main(String[] args) {
