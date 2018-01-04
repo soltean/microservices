6c9902ed3130eadfdb6dafa6211cac7a9dcab140
Sergiu Oltean <sergiu.c.oltean@gmail.com>
2018-01-04 21:32:40 +0200

    updates.

--- bid-service/bid-service-command-web/src/main/java/com/so/controller/BidController.java
+++ bid-service/bid-service-command-web/src/main/java/com/so/controller/BidController.java
@@ -13,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -45,25 +44,22 @@ public class BidController implements BidCommandFeignClient {
         return future.get();
     }
 
-    @RequestMapping(value = "/bidsHateoas", headers = HttpHeaders.ACCEPT, method = RequestMethod.POST, consumes = BidServiceMediaType.BID_SERVICE_BID_VALUE)
+    @RequestMapping(value = "/bidsHateoas", headers = HttpHeaders.ACCEPT, method = RequestMethod.POST)
     public ResponseEntity createBidWithHateoas(@RequestHeader(HttpHeaders.ACCEPT) String accept, @RequestBody BidRequest bid) throws ExecutionException, InterruptedException {
         logger.info("Creating bid " + bid);
-        //todo do I need this???
-        String type = accept.split("version=")[1];
-
         String version = accept.split("version=")[1];
 
         HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.CONTENT_TYPE, "must match the accept type");
+        headers.add(HttpHeaders.CONTENT_TYPE, BidServiceMediaType.BID_SERVICE_BID_VALUE + ";version=" + version);
         headers.add(HttpHeaders.VARY, "Content-Type");
         CompletableFuture<EntityWithIdAndVersion<BidAggregate>> future = bidService.addBid(bid.getItemCode(), bid.getAmount());
         EntityWithIdAndVersion result = future.get();
-        if (version.contains("1.0.0")) {
+        if (version.contains("1.9.0")) {
             CreateBidResourceV1 resource = new CreateBidResourceV1(result.getEntityId());
             resource.add(linkTo(methodOn(BidController.class).createBidWithHateoas(accept, bid)).withRel("bidsHateoas"));
             return new ResponseEntity(resource, headers, HttpStatus.OK);
         }
-        if (version.contains("2.0.5")) {
+        if (version.contains("2.1.0")) {
             CreateBidResourceV2 resource = new CreateBidResourceV2(result.getEntityId(), "I am Groot!");
             resource.add(linkTo(methodOn(BidController.class).createBidWithHateoas(accept, bid)).withRel("bidsHateoas"));
             return new ResponseEntity(resource, headers, HttpStatus.OK);
